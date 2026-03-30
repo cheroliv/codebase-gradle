1098a5b0ef8b9a9b89cb47afa343c9e2a8c62c86
cheroliv <cheroliv@laposte.net>
2026-03-30 15:37:45 +0200

    ✨ feat(buildSrc): add Kotlin DSL configuration and dependencies for YAML parsing and validation tools

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins { `kotlin-dsl` }
+
+repositories { mavenCentral() }
+
+dependencies {
+    setOf(
+        // YAML parsing
+        "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.21.2",
+        "com.fasterxml.jackson.module:jackson-module-kotlin:2.21.2",
+        // Bean Validation (ReadmeYmlAnonymizer @Email)
+        "jakarta.validation:jakarta.validation-api:3.1.0",
+        "org.hibernate.validator:hibernate-validator:8.0.1.Final",
+        "org.glassfish.expressly:expressly:5.0.0",
+        // Javelit UI chatbot
+        "io.javelit:javelit:0.86.0"
+    ).forEach(::implementation)
+}
+
